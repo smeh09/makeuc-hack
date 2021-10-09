04d82ba78663d9cb4ae2b9ad513ca2e8daadf416
YashLovesCode <yashtri22@gmail.com>
2021-10-09 14:43:39 +0530

    poop man shaktimaan

--- src/Components/BookSelect.js
+++ src/Components/BookSelect.js
@@ -17,6 +17,41 @@ const BookSelect = (props) => {
         getBook();
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [url])
+    function getSaleStuff(book){
+        if (book.saleInfo.saleability === "FOR_SALE"){
+            if (book.saleInfo.listPrice !== undefined){
+                if (book.saleInfo.buyLink !== undefined){
+                    return  (
+                    <div>
+                    <h2>Price: {`${book.saleInfo.listPrice.amount} INR`}</h2>
+                    <a  href={book.saleInfo.buyLink}>Link to buy your book</a>
+                    </div>
+                    )
+
+                }
+                else {
+                    return (
+                        <div>
+                        <h2>Price: {`${book.saleInfo.listPrice}`}</h2>
+                        <h2>Link: {`Not Available`}</h2>
+                        </div>
+                    )
+                }
+            }
+            else {
+                return (<div>
+                        <h2>Price: {`Not Available`}</h2>
+                        <h2>Link: {`Not Available`}</h2>
+                        </div>)
+            }
+        }
+        else {
+            return <h2>{`${book.saleInfo.saleability}`}</h2>
+        }
+    }
+    function getRating(book){
+        if (book.volumeInfo.averageRating ){}
+    }
     return (
         <div>
             {book.map((booker,i )=> (
@@ -24,7 +59,7 @@ const BookSelect = (props) => {
                     <h1>{booker.volumeInfo.title}</h1>
                     <h2>{booker.volumeInfo.authors}</h2>
                     <h2>{booker.volumeInfo.publisher}</h2>
-                    <h2>{booker.saleInfo.saleability}</h2>
+                    {getSaleStuff(booker)}
                     <h3>{booker.volumeInfo.categories}</h3>
                     <h3> Rating: {booker.volumeInfo.averageRating}  stars out of 5</h3>
                 </div>
