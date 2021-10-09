57519326de8cfbc26218718af70da8eaad9463b0
smeh09 <shauryameh09@gmail.com>
2021-10-09 15:52:24 +0530

    styles and styling touches

--- src/Components/HomeAppHero.js
+++ src/Components/HomeAppHero.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-
+import '../styles/HomeAppHeroMainContainer.css';
 import Button from '@mui/material/Button';
 import React from 'react';
 
@@ -9,16 +9,16 @@ export const HomeAppHero = () => {
       <div className='homeAppHeroLeftArea'>
         <div className='homeAppHeroHeadingContainer'>
           <h1 className='homeAppHeroHeading'>
-            Get, modern tech good quality projects, which will be built by our devs
+            Search for your favourite books, on the basis of author, title, etc.
           </h1>
           <p className='homeAppHeroDescr'>
-            We have good developers which will develop, good projects for you.
+            You can search your favourite books, and know about them here. We also have a dictionary to help you up with words! 
           </p>
-          <Link className='homeAppHeroAboutLink' to='/about'><Button className='homeAppHeroReadMoreBtn' variant="contained">Read more about us</Button></Link>
+          <Link className='homeAppHeroAboutLink' to='/searchBook'><Button className='homeAppHeroReadMoreBtn' variant="contained">Search for books</Button></Link>
         </div>
       </div>
       <div className='homeAppHeroRightArea'>
-        <img alt="Intro about us" className='homeAppHeroImg' src='https://bmsastech.com/wp-content/uploads/2020/04/mobile-app-development-technologies.png' />
+        <img alt="Intro about us" className='homeAppHeroImg' src='https://img.freepik.com/free-vector/woman-relaxing-home-girl-drinking-hot-tea-reading-book-armchair-flat-vector-illustration-leisure-evening-literature_74855-8684.jpg?size=626&ext=jpg' />
       </div>
     </div>
   )
