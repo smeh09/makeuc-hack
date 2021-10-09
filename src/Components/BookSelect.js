997c5f8ddb63bc0bfb20a7658048f0b9708657b5
smeh09 <shauryameh09@gmail.com>
2021-10-09 18:02:39 +0530

    styling of last page

--- src/Components/BookSelect.js
+++ src/Components/BookSelect.js
@@ -1,12 +1,14 @@
 import { useParams } from "react-router"
 import ApiCall from "../modules/ApiCall"
 import { useState, useEffect } from "react"
+import "../styles/BookSelect.css"
+
 const BookSelect = (props) => {
     const [book, setBook] = useState([])
     let params = useParams()
     console.log(params.id)
     let url = `https://www.googleapis.com/books/v1/volumes/${params.id}`
-    async function getBook () {
+    async function getBook() {
         console.log(url)
         let data = await ApiCall(url)
         console.log(data)
@@ -15,91 +17,94 @@ const BookSelect = (props) => {
     }
     useEffect(() => {
         getBook();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [url])
-    function getSaleStuff(book){
-        if (book.saleInfo.saleability === "FOR_SALE"){
-            if (book.saleInfo.listPrice !== undefined){
-                if (book.saleInfo.buyLink !== undefined){
-                    return  (
-                    <div>
-                    <h2>Price: {`${book.saleInfo.listPrice.amount} INR`}</h2>
-                    <a  href={book.saleInfo.buyLink}>Link to buy your book</a>
-                    </div>
+    function getSaleStuff(book) {
+        if (book.saleInfo.saleability === "FOR_SALE") {
+            if (book.saleInfo.listPrice !== undefined) {
+                if (book.saleInfo.buyLink !== undefined) {
+                    return (
+                        <div>
+                            <h2>Price: {`${book.saleInfo.listPrice.amount} INR`}</h2>
+                            <a href={book.saleInfo.buyLink}>`Link` to buy your book</a>
+                        </div>
                     )
 
                 }
                 else {
                     return (
                         <div>
-                        <h2>Price: {`${book.saleInfo.listPrice}`}</h2>
-                        <h2>Link: {`Not Available`}</h2>
+                            <h2>Price: {`${book.saleInfo.listPrice}`}</h2>
+                            <h2>Link: {`Not Available`}</h2>
                         </div>
                     )
                 }
             }
             else {
                 return (<div>
-                        <h2>Price: {`Not Available`}</h2>
-                        <h2>Link: {`Not Available`}</h2>
-                        </div>)
+                    <h2>Price: {`Not Available`}</h2>
+                    <h2>Link: {`Not Available`}</h2>
+                </div>)
             }
         }
         else {
             return <h2>{`${book.saleInfo.saleability}`}</h2>
         }
     }
-    function getRating(book){
-        if (book.volumeInfo.averageRating !== undefined){
+    function getRating(book) {
+        if (book.volumeInfo.averageRating !== undefined) {
             return `Rating: ${book.volumeInfo.averageRating} stars out of 5`
         }
         else {
             return `Rating: Not Available`
         }
     }
-    function getImage(book){
-        if (book.volumeInfo.imageLinks !== undefined){
+    function getImage(book) {
+        if (book.volumeInfo.imageLinks !== undefined) {
             return book.volumeInfo.imageLinks.thumbnail
         }
         else {
             return "https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg"
         }
     }
-    function getPreview(book){
-        if (book.accessInfo.accessViewStatus !== "NONE"){
+    function getPreview(book) {
+        if (book.accessInfo.accessViewStatus !== "NONE") {
             return (<div>
-                    <a href={`${book.accessInfo.webReaderLink}`}>Here is the preview link for your book</a>
-                    <h2>Note that previews may not work for some books</h2>
-                    </div>)
+                <a href={`${book.accessInfo.webReaderLink}`}>Here is the preview link for your book</a>
+                <h2>Note that previews may not work for some books</h2>
+            </div>)
         }
         else {
             return (<h2 > Preview is not available for this book</h2>)
 
-        } 
+        }
     }
 
     return (
-        <div>
-            {book.map((booker,i )=> (
-                <div key={i}>
-                <img src={getImage(booker)} alt="cover of your book"/>
-                    <h1>{booker.volumeInfo.title}</h1>
-                    <h2>{booker.volumeInfo.authors}</h2>
-                    <h2>{booker.volumeInfo.publisher}</h2>
-                    <h2 dangerouslySetInnerHTML={{__html: booker.volumeInfo.description}}></h2>
-                    <h3>{`Number of Pages in the ebook form: ${booker.volumeInfo.pageCount}`}</h3>
-                     <h3>{`Number of Pages in the print form: ${booker.volumeInfo.printedPageCount}`}</h3>
-                    {getSaleStuff(booker)}
-                    <h3>{booker.volumeInfo.categories}</h3>
-                    <h3>{getRating(booker)}</h3>
-                    {getPreview(booker)}
-                
+        <div className='bookData'>
+            {book.map((booker, i) => (
+                <div className='bookContainer' key={i}>
+                    <img className='bookImg' src={getImage(booker)} alt="cover of your book" />
+                    <div className='bookBottomArea'>
+                        <h1 className='bookTitle'>{booker.volumeInfo.title}</h1>
+                        <h2 className='bookAuthor'>Authors: {booker.volumeInfo.authors}</h2>
+                        <h2 className='bookPublishers'>Publishers: {booker.volumeInfo.publisher}</h2>
+                        <h1>Description: </h1>
+                        <p className='bookDescr' dangerouslySetInnerHTML={{ __html: booker.volumeInfo.description }}></p>
+                        <p>{`Number of Pages in the ebook form: ${booker.volumeInfo.pageCount}`}</p>
+                        <p>{`Number of Pages in the print form: ${booker.volumeInfo.printedPageCount}`}</p>
+                        <div className='link'>{getSaleStuff(booker)}</div>
+                        <hr />
+                        <h2 className='bookSaleInfo'>SALE INFO: {booker.volumeInfo.categories}</h2>
+                        <h3>{getRating(booker)}</h3>
+                        <div className='link'>{getPreview(booker)}</div>
+                    </div>
                 </div>
             ))}
-            
+
         </div>
     )
-    
+
 }
 
 export default BookSelect
