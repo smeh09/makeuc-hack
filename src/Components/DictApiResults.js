237856b76445e8be3f3c71b314771b85cbdcb9e6
YashLovesCode <yashtri22@gmail.com>
2021-10-10 07:55:07 +0530

    ho

--- src/Components/DictApiResults.js
+++ src/Components/DictApiResults.js
@@ -3,17 +3,17 @@ import { useState, useEffect } from "react";
 import CleanUrlDict from "../modules/CleanUrlDict"
 import { useParams } from "react-router-dom";
 import "../styles/DictApiResults.css"
-
 const DictApiResults = (props) => {
 
     let params = useParams()
     const [meaning, setMeaning] = useState([])
     let url = CleanUrlDict(params.lang, params.word)
-
     async function getMeaning() {
+      
         let data = await fetch(url, { mode: "cors" })
         let json = await data.json()
         setMeaning(json)
+        
     }
 
     useEffect(() => {
@@ -22,7 +22,7 @@ const DictApiResults = (props) => {
     }, [url])
 
     console.log(meaning)
-
+    if (meaning.title !== "No Definitions Found"){
     return (
         <div>
             {
@@ -48,6 +48,15 @@ const DictApiResults = (props) => {
                 })
             }
         </div>
-    )
+    )}
+    else {
+        return (
+            <div>
+            <h2>{meaning.title}</h2>
+            <img src="https://cdn.dribbble.com/users/1554526/screenshots/3399669/no_results_found.png" alt="error "/>
+            <p>This likely happened because the word you typed does not exist in the language you have chosen, please try again with a different word or you can try searching the web</p>
+            </div>
+        )
+    }
 }
 export default DictApiResults;
