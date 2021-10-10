e9cdf2de8cbbf9815bb7f8c8c3840c07a0a44a23
smeh09 <shauryameh09@gmail.com>
2021-10-10 12:41:37 +0530

    styling touches

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -10,13 +10,13 @@ export const Cards = () => {
     <div className='cards'>
       <div className='card'>
       <h2 className='cardHeading'>A book finder</h2>
-        <p className="cardDescr">In this project, we have a word book finder. You can give, multiple attributes to find books like, general keywords, book title, author, publisher, etc. From these attributes you give, we find you options of suitable books you are looking for!</p>
-        <div className='cardLinkContainer'><Link className='cardLink' to='/searchBook'><Button className='cardBtn' variant="contained">Search books</Button></Link></div>
+        <p className="cardDescr">In this project, we have a word book finder. You can give, multiple attributes to find books like, general keywords, book title, etc. From these attributes you give, we find you options of suitable books you are looking for!</p>
+        <div className='cardLinkContainer'><Link className='cardLink' to='/searchBook'><Button className='cardBtn' variant="contained">Know More</Button></Link></div>
       </div>
       <div className='card'>
         <h2 className='cardHeading'>A word meaning finder</h2>
-        <p className="cardDescr">In this project, we also have a word meaning finder! It supports multiple languages like - English, French, German and Spanish! You can search for a word's meaning, and we'll show you with the meaning of that word, example sentence, and much more. </p>
-        <div className='cardLinkContainer'><Link className='cardLink' to='/searchDictionary'><Button className='cardBtn' variant="contained">Search word meanings</Button></Link></div>
+        <p className="cardDescr">In this project, we also have a word meaning finder! It supports multiple languages too! You can search for a word's meaning, and we'll show you it's meaning, example, etc. </p>
+        <div className='cardLinkContainer'><Link className='cardLink' to='/searchDictionary'><Button className='cardBtn' variant="contained">Know More</Button></Link></div>
       </div>
     </div>
     </>
--- src/Components/FeatureOne.js
+++ src/Components/FeatureOne.js
@@ -5,7 +5,6 @@ export const FeatureOne = () => {
   return (
     <div className='feature1'>
       <div className='feature1TopArea'>
-        <p className='feature1HeadingSmall'>SEARCH BOOKS AND WORD MEANINGS</p>
         <h1 className='feature1Heading'>You can search for books and get meanings of words from here! </h1>
         <p className="feature1Descr">
           Search up book on your favourite topic, or on the topic you want help with. If can't understand a word use our word meaning finder! 
